e84cb5e6bfaac25515573b113480b80568255340
Suor <suor.web@gmail.com>
2012-06-09 11:54:38 +0800

    Precompile regexes in StripWhitespace middleware
    Mostly useless since compiled regex objects are cached
    by re module.

--- handy/middleware.py
+++ handy/middleware.py
@@ -5,10 +5,13 @@ class StripWhitespace(object):
     """
     Strips leading and trailing whitespace from response content.
     """
+    NEWLINE_RE = re.compile(r'>\s*\n\s*<')
+    SPACES_RE = re.compile(r'>\s{2,}<')
+
     def process_response(self, request, response):
         if response['Content-Type'].startswith('text/html'):
             content = response.content
-            content = re.sub(r'>\s*\n\s*<', '>\n<', content)
-            content = re.sub(r'>\s{2,}<', '> <', content)
+            content = self.NEWLINE_RE.sub('>\n<', content)
+            content = self.SPACES_RE.sub('> <', content)
             response.content = content
         return response
