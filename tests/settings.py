2291872cc90a7732ce3cea16ecd787e4eed71f2c
Alexander Schepanovski <suor.web@gmail.com>
2012-12-15 00:33:28 +0800

    Setup tests

---
+++ tests/settings.py
@@ -0,0 +1,17 @@
+INSTALLED_APPS = [
+    'tests',
+]
+
+# Django replaces this, but it still wants it. *shrugs*
+DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2',
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'handy',
+        'USER': 'handy',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        # 'OPTIONS': {"autocommit": True},
+    }
+}
