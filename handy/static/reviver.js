c40f2e4d3cc655ece34f09280140a7f05de89501
Suor <suor.web@gmail.com>
2013-02-27 10:55:36 +0800

    Added javascript revivers

---
+++ handy/static/reviver.js
@@ -0,0 +1,15 @@
+// Microframework for reviving objects serialized by django
+JSON.revive = function (text) {
+    return JSON.parse(text, JSON.reviver);
+};
+
+JSON.reviver = function reviver(key, value) {
+    if (value && typeof value === 'object' && value._type) {
+        return JSON.reviver[value._type](value);
+    }
+    return value;
+};
+
+JSON.reviver.date = function (value) {
+    return new Date(value.ms);
+};
