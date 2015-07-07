6e146c7e482139d0ee71e94add7de326703830c7
Alexander Schepanovski <suor.web@gmail.com>
2015-07-07 17:46:14 +0700

    Fix @render_to_json() and @ajax for newer Djangos

--- handy/decorators.py
+++ handy/decorators.py
@@ -72,7 +72,7 @@ def render_to_json(ensure_ascii=True, default=_json_default):
                     json_data = json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, default=default)
                 else:
                     json_data = json.dumps(response, ensure_ascii=ensure_ascii, separators=(',',':'), default=default)
-            return HttpResponse(json_data, mimetype='application/json; charset=utf-8')
+            return HttpResponse(json_data, content_type='application/json')
         return wrapper
     return decorator
 
