da9b9d223abfbcca018c2b94931108011bd9360f
Alexander Schepanovski <suor.web@gmail.com>
2016-10-22 12:22:17 +0700

    Use better @cached_property implementation
    Supports assignment and deletion. More efficient.

--- handy/utils.py
+++ handy/utils.py
@@ -60,15 +60,24 @@ def get_module_attr(path):
         return None
 
 
-def cached_property(func):
-    @property
-    @wraps(func)
-    def wrapper(self):
-        attname = '_' + func.__name__
-        if not hasattr(self, attname):
-            setattr(self, attname, func(self))
-        return getattr(self, attname)
-    return wrapper
+class cached_property(object):
+    """
+    Decorator that converts a method with a single self argument into
+    a property cached on the instance.
+    """
+    # NOTE: implementation borrowed from Django.
+    # NOTE: we use fget, fset and fdel attributes to mimic @property.
+    fset = fdel = None
+
+    def __init__(self, fget):
+        self.fget = fget
+        self.__doc__ = getattr(fget, '__doc__')
+
+    def __get__(self, instance, type=None):
+        if instance is None:
+            return self
+        res = instance.__dict__[self.fget.__name__] = self.fget(instance)
+        return res
 
 
 def get_or_none(cls, **cond):
